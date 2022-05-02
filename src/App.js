97da0054995a8c54f8e2fbeeba5f6792b902f3dd
Ian Bravyn <ianbravynsa@gmail.com>
2022-05-02 11:49:38 +0300

    This is a web app that lets you manage your family

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+import logo from './mafamily.png';
 import './App.css';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
@@ -7,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Connect better with your family.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Join a Family now!
+          <SignIn/>
+        
       </header>
     </div>
   );
